3cb99918b61174fccd90959b35e3aac2b1ff044d
jack1eljoker1 <74050446+jack1eljoker1@users.noreply.github.com>
2021-05-16 19:38:37 +0200

    Add files via upload

---
+++ misc.py
@@ -0,0 +1,19 @@
+HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
+
+**/help** __Show This Message.__
+**/skip** __Skip The Current Playing Music.__
+**/play** __youtube/saavn/deezer Song_Name__
+**/telegram** __Play From Telegram Audio.__
+**/joinvc** __Join Voice Chat.__
+**/leavevc** __Leave Voice Chat.__
+**/volume [1-200]** __Adjust Volume.__
+**/pause** __Pause Music.__
+**/resume** __Resume Music.__
+**/update** __Update & Restart.__"""
+
+START_TEXT = "__**Hi I'm Telegram Voice Chat Bot. Join @JACK_BOTS For Support.**__"
+
+REPO_TEXT = (
+    "[Github](https://github.com/jack1eljoker1/Telegram_vc_bot)"
+    + " | [CH](t.me/JACK_BOTS)"
+)
